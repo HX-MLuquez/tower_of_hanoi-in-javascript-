67ab5a86a6c9a93ea864906300d1f55540e470c9
Mauricio Gastón <mauuuricio@hotmail.com>
2024-02-08 18:01:50 -0300

    fixed game

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import code_tower_hanoi from "./utils/code_tower_hanoi";
 
 const App = () => {
   const [numAros, setNumAros] = useState(0);
-  const [linesAros, setLinesAros] = useState([]);
+
   const [steps, setSteps] = useState(0);
   const [objSolution, setObjSolution] = useState({});
+  const [clickOnOff, setClickOnOff] = useState(false);
+  const [firstMove, setFirstMove] = useState("");
+  const [finish, setFinish] = useState(false);
 
-  // code_tower_hanoi(9, "A", "B", "C");
+  const [lines, setLines] = useState({
+    origin: [],
+    aux: [],
+    destiny: [],
+  });
 
   const handleInputChange = (event) => {
     const num = parseInt(event.target.value);
@@ -38,21 +45,85 @@ const App = () => {
       );
       cant++;
     }
-    setLinesAros(aros);
+
+    setLines((l) => {
+      return {
+        ...l,
+        origin: aros,
+      };
+    });
     return;
   };
 
   function initCodeTowerHanoi() {
+    if (Object.keys(objSolution).length > 0) {
+      alert(
+        "El juego ya se ha iniciado \n Si quieres iniciar de nuevo primero selecciona 'Reiniciar'"
+      );
+      return;
+    }
     const result = code_tower_hanoi(numAros, "A", "B", "C");
-    console.log(result);
+
     setObjSolution(result);
   }
   function reset() {
     setNumAros(0);
-    setLinesAros([]);
+    setLines({
+      origin: [],
+      aux: [],
+      destiny: [],
+    });
     setSteps(0);
     setObjSolution({});
+    setFinish(false);
     return;
+  }
+
+  function movePiece(e) {
+    if (finish)
+      return alert(
+        "Felicidades!!!! Lo has conseguido!!! \n Si quieres volver a jugar selecciona 'Reiniciar'"
+      );
+    const move = e.currentTarget.getAttribute("name");
+
+    if (!clickOnOff) {
+      if (lines[move].length === 0)
+        return alert(`Esta columna ${move} no contiene ningún aro`);
+      setFirstMove(move);
+      setClickOnOff(true);
+    } else {
+      if (
+        lines[move].length === 0 ||
+        lines[move][lines[move].length - 1].props.children.length >
+          lines[firstMove][lines[firstMove].length - 1].props.children.length
+      ) {
+        var newLines = [...lines[firstMove]];
+        var piece = newLines.shift();
+        setLines((l) => {
+          return {
+            ...l,
+            [firstMove]: newLines,
+            [move]: [piece, ...l[move]],
+          };
+        });
+
+        if (move === "destiny" && lines[move].length + 1 === numAros) {
+          setFinish(true);
+          alert(
+            "Felicidades!!!! Lo has conseguido!!! \n Si quieres volver a jugar selecciona 'Reiniciar'"
+          );
+          return;
+        }
+      } else {
+        alert(
+          "La pieza seleccionada no puede ser mayor a la que se encuentra donde alojar"
+        );
+        return;
+      }
+
+      setFirstMove("");
+      setClickOnOff(false);
+    }
   }
   return (
     <div className="app">
@@ -73,36 +144,38 @@ const App = () => {
                 value={numAros}
               />
             </div>
-            <button onClick={initCodeTowerHanoi}>Aplicar</button>
+            <button onClick={initCodeTowerHanoi}>Aplicar Solución</button>
             <button onClick={reset}>Reiniciar</button>
           </div>
         </div>
-        <div className="box">
+        <div className="box center" onClick={movePiece} name="origin">
           <h4>ORIGEN</h4>
           <div className="torre">
-            {linesAros &&
-              linesAros.map((l, i) => {
+            {lines.origin &&
+              lines.origin.map((l, i) => {
                 return l;
               })}
           </div>
           <div key={"100"}>█████</div>
           <h4>A</h4>
         </div>
-        <div className="box thin">
+        <div className="box center" onClick={movePiece} name="aux">
           <h4>AUXILIAR</h4>
-          {linesAros &&
-            linesAros.map((l, i) => {
-              return <div key={i}>█</div>;
+          {lines.aux &&
+            lines.aux.map((l, i) => {
+              return <div key={i}>{l}</div>;
             })}
+
           <div key={"101"}>█████</div>
           <h4>B</h4>
         </div>
-        <div className="box thin">
+        <div className="box center" onClick={movePiece} name="destiny">
           <h4>DESTINO</h4>
-          {linesAros &&
-            linesAros.map((l, i) => {
-              return <div key={i}>█</div>;
+          {lines.destiny &&
+            lines.destiny.map((l, i) => {
+              return <div key={i}>{l}</div>;
             })}
+
           <div key={"102"}>█████</div>
           <h4>C</h4>
         </div>
@@ -133,15 +206,14 @@ const App = () => {
             moviendo estos discos de acuerdo con las reglas inmutables de Brahma
             desde ese momento.
           </p>
-          
+
           <p>
             Por lo tanto, el acertijo también se conoce como el rompecabezas de
             la Torre de Brahma. Según la leyenda, cuando se complete el último
             movimiento del rompecabezas, el mundo se terminará.
           </p>
           <h4>
-            Code en:
-            https://github.com/HX-MLuquez/tower_of_hanoi-in-javascript-
+            Code en: https://github.com/HX-MLuquez/tower_of_hanoi-in-javascript-
           </h4>
         </div>
       </div>
